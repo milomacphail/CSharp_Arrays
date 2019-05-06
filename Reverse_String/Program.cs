13af7e943ba3a6d044df0d2752f057ba7080ab3c
milomacphail <milomacphail@gmail.com>
2019-05-06 19:04:01 -0400

    Reverse String

---
+++ Reverse_String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Reverse_String
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter a string: ");
+
+            var input = Console.ReadLine();
+
+            var stringInput = new char[input.Length];
+
+            for (int letter = input.Length; letter > 0; letter--)
+            {
+                stringInput[input.Length - letter] = input[letter - 1];
+            }
+
+            var stringReversed = new string(stringInput);
+            Console.WriteLine(stringReversed);
+            
+        }
+    }
+}
